46787078926161947997bad1216f801461c6a616
adelinepclarke <aclarke5@kpmg.com.au>
2019-01-10 15:49:23 +1100

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,19 @@
+# data is downloaded, upzipped, and the .txt file is in the working directory
+
+power <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
+
+# extract specific dates as required, then bind these
+power1 <- subset(power, Date=="1/2/2007")
+power2 <- subset(power, Date=="2/2/2007")
+powerdata <- rbind(power1,power2)
+
+#change date format
+powerdata$Date <- as.Date(powerdata$Date, format="%d/%m/%Y")
+
+#build histogram
+hist(as.numeric(powerdata$Global_active_power), col="Red", breaks=12, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
+
+#copy plot to png file
+dev.copy(png, file="plot1.png", width=480, height=480)
+
+dev.off()
